b1f2bf22d079463fa45933439a2db9be14dbf466
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2020-01-10 01:10:04 +0100

    added helper functions

--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE FlexibleContexts #-}
-module Helpers where
+module Helpers (notExists, parentOfGroup) where
 
 import           Types
 import           Example1
@@ -9,11 +7,21 @@ import           Control.Lens
 import qualified Data.Set                      as S
 import qualified Data.Map                      as M
 
-notExists :: FeatureID -> FeatureModel -> Bool
-notExists fid = hasn't (features . ix fid)
+notExists :: FeatureID -> FeatureTable -> Bool
+notExists fid = hasn't (ix fid)
+
+parentOfGroup :: GroupID -> FeatureTable -> Maybe FeatureID
+parentOfGroup gid =
+  preview (itraversed . filtered (has (groups . ix gid)) . asIndex)
 
 main :: IO ()
 main = do
   let printDivider s = putStr "\n\n\n--- " >> putStr s >> putStrLn "\n"
+      carFeatureTable = view features carExample
   print "hei"
+  print $ carFeatureTable & notExists 2
+  print $ carFeatureTable & notExists 4
+  print $ carFeatureTable & parentOfGroup 21
+  print $ carFeatureTable & parentOfGroup 22
+  print $ carFeatureTable & parentOfGroup 11
 
