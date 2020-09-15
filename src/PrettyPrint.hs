31863dd9f7808194c3e9b287814e4204afeb09c9
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2020-09-15 13:54:55 +0200

    basic prettyprint for featuremodel

--- src/PrettyPrint.hs
+++ src/PrettyPrint.hs
@@ -2,32 +2,84 @@ module PrettyPrint where
 
 import           Types
 import           Data.Text.Prettyprint.Doc
-import           Control.Lens
 import           Data.Text.Prettyprint.Doc.Util
+import qualified Data.Map                      as M
+import qualified Data.Set                      as S
 
 prettyType :: [Doc ann] -> Doc ann
-prettyType = align . sep . zipWith (<+>) (":::" : repeat "->")
+prettyType =
+  align . sep . zipWith (<+>) (":::" : repeat "->")
 
 prettyDecl :: Pretty a => a -> [Doc ann] -> Doc ann
 prettyDecl n tys = pretty n <+> prettyType tys
 
 doc :: Doc ann
-doc = prettyDecl ("example" :: String) ["Int", "Bool", "Char", "IO ()"]
+doc = prettyDecl ("example" :: String)
+                 ["Int", "Bool", "Char", "IO ()"]
 
 printThings :: IO ()
-printThings =
-  putDocW 30 doc
+printThings = putDocW 50 doc
 
 printFeatureModel :: FeatureModel -> IO ()
-printFeatureModel = do
-  const (print "hei")
-  putDocW 30 . prettyFeatureModel
+printFeatureModel fm = do
+  putDocW 50 (prettyFeatureModel fm)
+  putStrLn ""
 
 prettyFeatureModel :: FeatureModel -> Doc ann
-prettyFeatureModel (FM r f) = "FEATURE MODEL" <> hardline <> 
-  indent 2 (vsep ["rootId: " <> pretty r, "features:", indent 2 (prettyFeatureTable f)])
+prettyFeatureModel (FM r f) =
+  "FEATURE MODEL" <> line <> indent
+    2
+    (vsep
+      [ "rootId: " <> pretty r
+      , "features:"
+      , indent 2 (prettyFeatureTable f)
+      ]
+    )
 
 prettyFeatureTable :: FeatureTable -> Doc ann
-prettyFeatureTable ft = align (vsep ((\(k,v) -> (pretty k <+> "->" <+> pretty (v^.name) )) <$> kvs))
+prettyFeatureTable ft = align
+  (vsep
+    (   (\(k, v) -> (pretty k <+> "->" <+> prettyFeature v))
+    <$> M.toList ft
+    )
+  )
+
+prettyFeature :: Feature -> Doc ann
+prettyFeature (Feature name parent groups featureType) =
+  asList $ prettyArgs
+    "="
+    [ ("name", pretty name)
+    , ( "parentGroupId"
+      , maybe "[No Parent Group]" pretty parent
+      )
+    , ("groups"     , prettyGroups groups)
+    , ("featureType", pretty (show featureType))
+    ]
+
+prettyGroups :: Groups -> Doc ann
+prettyGroups = mconcat . fmap (\g -> line <> prettyGroup g) . M.toList
+
+prettyGroup :: (GroupId, Group) -> Doc ann
+prettyGroup (gid, Group gType featureIds) =
+  tupled 
+    [ pretty gid
+    , pretty (show gType)
+    , asCompactSet (pretty <$> S.toList featureIds)
+    ]
+
+prettyArgs :: Doc ann -> [(Doc ann, Doc ann)] -> [Doc ann]
+prettyArgs sep xs = map makeLine xs
   where
-    kvs = itoListOf itraversed ft
+    makeLine (key, value) =
+      key <+> sep <> nest 4 (softline <> value)
+
+
+asList :: [Doc ann] -> Doc ann
+asList = group . align . encloseSep
+  (flatAlt "[ " "[")
+  (flatAlt (line <> "]") "]")
+  ", "
+
+asCompactSet :: [Doc ann] -> Doc ann
+asCompactSet xs = "{" <> align
+  (concatWith (\x y -> x <> "," <> softline <> y) xs <> "}")
