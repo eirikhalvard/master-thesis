23858e03da63a81f05c73ba66473b2107252a06b
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2020-12-03 17:03:47 +0100

    first test case:)

--- backend/test/Spec.hs
+++ backend/test/Spec.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE OverloadedLists #-}
+
 import Control.Exception (evaluate)
+import Control.Lens
+import Example
+import qualified Lenses as L
+import qualified Merge.ChangeDetection as ChangeDetection
 import Test.Hspec
 import Test.QuickCheck
+import Types
 
 main :: IO ()
 main = hspec $ do
@@ -13,3 +20,33 @@ main = hspec $ do
 
     it "throws an exception if used with an empty list" $ do
       evaluate (head []) `shouldThrow` anyException
+
+  describe "Change Detection" $ do
+    it "deriving changes at the first timepoint correctly" $ do
+      preview (L.plans . ix 0) (ChangeDetection.deriveChanges baseEvolutionPlan)
+        `shouldBe` Just
+          ( Plan
+              1
+              ( Modifications
+                  [
+                    ( "feature:vending-machine"
+                    , FeatureModification Nothing Nothing (Just (FeatureNameModification "VendingMachine"))
+                    )
+                  ,
+                    ( "feature:beverages"
+                    , FeatureAdd (Just "group:vending-machine-group") Mandatory "Beverages"
+                    )
+                  ,
+                    ( "feature:tea"
+                    , FeatureAdd (Just "group:beverages-group") Optional "Tea"
+                    )
+                  ,
+                    ( "feature:coffee"
+                    , FeatureAdd (Just "group:beverages-group") Optional "Coffee"
+                    )
+                  ]
+                  [ ("group:vending-machine-group", GroupAdd "feature:vending-machine" And)
+                  , ("group:beverages-group", GroupAdd "feature:beverages" Or)
+                  ]
+              )
+          )
