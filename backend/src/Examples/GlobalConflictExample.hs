2e5557935c35a7d1f4c1c046cbe7856fa33ce974
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2021-02-02 01:06:11 +0100

    more global examples

--- backend/src/Examples/GlobalConflictExample.hs
+++ backend/src/Examples/GlobalConflictExample.hs
@@ -1,6 +1,90 @@
 module Examples.GlobalConflictExample where
 
+import Control.Lens
+
 import Examples.SoundExample
+import qualified Lenses as L
+import Types
+
+-- Moving group to form cycle
+-- v1 is changing the parent of an addGroup
+-- while v2 is changing the parent of an existing group
+-- each group is moved to a child feature of the other group
+-- which forms a cycle
+groupMoveCycle :: MergeInputData FlatModificationEvolutionPlan
+groupMoveCycle =
+  MergeInputData
+    "Conflict, Global - Group Move Cycle"
+    baseConstructedEvolutionPlan
+    ( v1ConstructedEvolutionPlan
+        & L.plans
+          . traversed
+          . filtered (has $ L.timePoint . only 3)
+          . L.transformation
+          . L.groups
+          . ix "group:milk-group"
+          %~ \(GroupAdd _ gType) -> GroupAdd "feature:regular" gType
+    )
+    ( v2ConstructedEvolutionPlan
+        & L.plans
+          . traversed
+          . filtered (has $ L.timePoint . only 3)
+          . L.transformation
+          . L.groups
+          . at "group:size-group"
+          ?~ GroupModification (Just $ GroupParentModification "feature:normal") Nothing
+    )
+    ( Just $
+        Left $
+          Global
+            3
+            ( FailedDependencies
+                [ GroupDependency
+                    (GroupModification (Just $ GroupParentModification "feature:normal") Nothing)
+                    (NoCycleFromGroup "group:size-group")
+                ]
+            )
+    )
+
+-- Moving group to form cycle
+-- v1 is changing the parent of an addGroup
+-- while v2 is changing the parent of an existing group
+-- each group is moved to a child feature of the other group
+-- which forms a cycle
+
+violatingFeatureWellFormed :: MergeInputData FlatModificationEvolutionPlan
+violatingFeatureWellFormed =
+  MergeInputData
+    "Conflict, Global - Well-formedness violation"
+    baseConstructedEvolutionPlan
+    v1ConstructedEvolutionPlan
+    ( v2ConstructedEvolutionPlan
+        & L.plans
+          . traversed
+          . filtered (has $ L.timePoint . only 3)
+          . L.transformation
+          . L.groups
+          . at "group:vending-machine-group"
+          ?~ GroupModification Nothing (Just $ GroupTypeModification Or)
+        & L.plans
+          . traversed
+          . filtered (has $ L.timePoint . only 3)
+          . L.transformation
+          . L.features
+          . at "feature:beverages"
+          ?~ FeatureModification Nothing (Just $ FeatureTypeModification Optional) Nothing
+    )
+    ( Just $
+        Left $
+          Global
+            3
+            ( FailedDependencies
+                [ GroupDependency
+                    (GroupModification Nothing (Just $ GroupTypeModification Or))
+                    (GroupIsWellFormed "group:size-group")
+                ]
+            )
+    )
 
 -- data Conflict
 --   = Merge Time MergeConflict
