06de80c663664297180470b15799f8870d589d8f
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2021-02-09 14:59:16 +0100

    merge conflict errors

--- backend/src/Conflict.hs
+++ backend/src/Conflict.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Conflict where
 
-import Data.Text.Lazy
-import Text.Pretty.Simple
+import Data.List (intercalate)
+import Data.Maybe (catMaybes)
 
 import Types
+
+------------------------------------------------------------------------
+--                      CONFLICT ERROR MESSAGES                       --
+------------------------------------------------------------------------
+
+class ConflictShow a where
+  toErrorMessage :: a -> String
 
 conflictErrorMsg :: Conflict -> String
 conflictErrorMsg conflict =
@@ -17,9 +26,6 @@ conflictErrorMsg conflict =
     Panic time str ->
       constructErrorMsg time $ "The algorithm paniced! with message:\n" ++ str
 
-class ConflictShow a where
-  toErrorMessage :: a -> String
-
 constructErrorMsg :: Int -> String -> String
 constructErrorMsg time reason =
   "A conflict occured at time\n"
@@ -28,20 +34,29 @@ constructErrorMsg time reason =
     ++ "The conflict occured because of "
     ++ reason
 
+------------------------------------------------------------------------
+--                           MERGE CONFLICT                           --
+------------------------------------------------------------------------
+
 instance ConflictShow MergeConflict where
   toErrorMessage (FeatureConflict id bothChange) =
-    toErrorMessage bothChange
+    toErrorMessage (id, bothChange)
   toErrorMessage (GroupConflict id bothChange) =
-    toErrorMessage bothChange
+    toErrorMessage (id, bothChange)
 
-instance ConflictShow modificationType => ConflictShow (BothChange modificationType) where
-  toErrorMessage bothChange =
+instance
+  ConflictShow modificationType =>
+  ConflictShow (String, BothChange modificationType)
+  where
+  toErrorMessage (id, bothChange) =
     case bothChange of
       BothChangeWithBase
         baseModification
         removedOrChangedV1
         removedOrChangedV2 ->
-          "conflicting operations\n"
+          "conflicting operations for "
+            ++ id
+            ++ "\n"
             ++ "The base version originally had the following operation:\n"
             ++ toErrorMessage baseModification
             ++ "And version 1 tried to \n"
@@ -54,8 +69,10 @@ instance ConflictShow modificationType => ConflictShow (BothChange modificationT
           "conflicting operations\n"
             ++ "Version 1 tried to add the following operation\n"
             ++ toErrorMessage v1Modification
+            ++ "\n"
             ++ "Version 2 tried to add the following operation\n"
             ++ toErrorMessage v2Modification
+            ++ "\n"
     where
       removeChangeHelper RemovedModification =
         "remove the operation\n"
@@ -66,12 +83,56 @@ instance ConflictShow modificationType => ConflictShow (BothChange modificationT
 instance ConflictShow FeatureModification where
   toErrorMessage featureModification =
     case featureModification of
-      FeatureAdd parentGroupId featureType name -> undefined
-      FeatureRemove -> undefined
-      FeatureModification mParent mType mName -> undefined
+      FeatureAdd parentGroupId featureType name ->
+        "Feature Addition"
+          ++ ": parent = "
+          ++ parentGroupId
+          ++ ", type = "
+          ++ show featureType
+          ++ ", name = "
+          ++ name
+      FeatureRemove -> "Feature Removal"
+      FeatureModification mParent mType mName ->
+        "Feature Modification: "
+          ++ ( intercalate ", " . catMaybes $
+                [ fmap (("parent = " ++) . toErrorMessage) mParent
+                , fmap (("type = " ++) . toErrorMessage) mType
+                , fmap (("name = " ++) . toErrorMessage) mName
+                ]
+             )
+
+instance ConflictShow FeatureParentModification where
+  toErrorMessage (FeatureParentModification parent) = parent
+
+instance ConflictShow FeatureTypeModification where
+  toErrorMessage (FeatureTypeModification featureType) = show featureType
+
+instance ConflictShow FeatureNameModification where
+  toErrorMessage (FeatureNameModification featureName) = featureName
 
 instance ConflictShow GroupModification where
-  toErrorMessage groupModification = show groupModification
+  toErrorMessage groupModification =
+    case groupModification of
+      GroupAdd parentGroupId groupType ->
+        "Group Addition"
+          ++ ": parent = "
+          ++ parentGroupId
+          ++ ", type = "
+          ++ show groupType
+      GroupRemove -> "Group Removal"
+      GroupModification mParent mType ->
+        "Group Modification: "
+          ++ ( intercalate ", " . catMaybes $
+                [ fmap (("parent = " ++) . toErrorMessage) mParent
+                , fmap (("type = " ++) . toErrorMessage) mType
+                ]
+             )
+
+instance ConflictShow GroupParentModification where
+  toErrorMessage (GroupParentModification groupParent) = groupParent
+
+instance ConflictShow GroupTypeModification where
+  toErrorMessage (GroupTypeModification groupType) = show groupType
 
 instance ConflictShow LocalConflict where
   toErrorMessage localConflict = show localConflict
@@ -79,16 +140,12 @@ instance ConflictShow LocalConflict where
 instance ConflictShow GlobalConflict where
   toErrorMessage globalConflict = show globalConflict
 
+------------------------------------------------------------------------
+--                           LOCAL CONFLICT                           --
+------------------------------------------------------------------------
+
 constructLocalConflict :: LocalConflict -> String
 constructLocalConflict localConflict = show localConflict
-
-constructGlobalConflict :: GlobalConflict -> String
-constructGlobalConflict globalConflict = show globalConflict
-
--- data MergeConflict
---   = FeatureConflict (BothChange FeatureModification)
---   | GroupConflict (BothChange GroupModification)
---   deriving (Show, Eq, Read)
 
 -- data LocalConflict
 --   = FeatureAlreadyExists FeatureModification FeatureId
@@ -96,6 +153,13 @@ constructGlobalConflict globalConflict = show globalConflict
 --   | GroupAlreadyExists GroupModification GroupId
 --   | GroupNotExists GroupModification GroupId
 --   deriving (Show, Eq, Read)
+
+------------------------------------------------------------------------
+--                          GLOBAL CONFLICT                           --
+------------------------------------------------------------------------
+
+constructGlobalConflict :: GlobalConflict -> String
+constructGlobalConflict globalConflict = show globalConflict
 
 -- data GlobalConflict
 --   = FailedDependencies [Dependency]
