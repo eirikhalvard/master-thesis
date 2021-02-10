2cad6cd732f96cb06d915c950dcdaf184ca57bd5
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2021-02-10 23:25:28 +0100

    FromJSON ToJSON deriving

--- backend/src/Types.hs
+++ backend/src/Types.hs
@@ -133,13 +133,13 @@ data TransformationEvolutionPlan transformation featureModel = TransformationEvo
   , _initialFM :: featureModel
   , _plans :: [Plan transformation]
   }
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data Plan transformation = Plan
   { _timePoint :: Time
   , _transformation :: transformation
   }
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 type ModificationEvolutionPlan featureModel = TransformationEvolutionPlan Modifications featureModel
 
@@ -173,7 +173,7 @@ data Modifications = Modifications
   { _features :: M.Map FeatureId FeatureModification
   , _groups :: M.Map GroupId GroupModification
   }
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data FeatureModification
   = FeatureAdd GroupId FeatureType String
@@ -182,19 +182,19 @@ data FeatureModification
       (Maybe FeatureParentModification)
       (Maybe FeatureTypeModification)
       (Maybe FeatureNameModification)
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data FeatureParentModification
   = FeatureParentModification GroupId
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data FeatureNameModification
   = FeatureNameModification String
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data FeatureTypeModification
   = FeatureTypeModification FeatureType
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data GroupModification
   = GroupAdd FeatureId GroupType
@@ -202,15 +202,15 @@ data GroupModification
   | GroupModification
       (Maybe GroupParentModification)
       (Maybe GroupTypeModification)
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data GroupParentModification
   = GroupParentModification FeatureId
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data GroupTypeModification
   = GroupTypeModification GroupType
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 --- DIFF RESULT ---
 
@@ -220,7 +220,7 @@ data DiffResult = DiffResult
   { _features :: M.Map FeatureId FeatureDiffResult
   , _groups :: M.Map GroupId GroupDiffResult
   }
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 type FeatureDiffResult =
   SingleDiffResult FeatureModification
@@ -233,7 +233,7 @@ data SingleDiffResult modificationType
   = NoChange modificationType
   | ChangedInOne Version (OneChange modificationType)
   | ChangedInBoth (BothChange modificationType)
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data OneChange modificationType
   = OneChangeWithBase
@@ -241,7 +241,7 @@ data OneChange modificationType
       (RemovedOrChangedModification modificationType) -- Derived (V1 or V2) modification
   | OneChangeWithoutBase
       (AddedModification modificationType) -- Derived (V1 or V2) modification
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data BothChange modificationType
   = BothChangeWithBase
@@ -251,21 +251,21 @@ data BothChange modificationType
   | BothChangeWithoutBase
       (AddedModification modificationType) -- V1 modification
       (AddedModification modificationType) -- V2 modification
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data RemovedOrChangedModification modificationType
   = RemovedModification
   | ChangedModification modificationType
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data AddedModification modificationType
   = AddedModification modificationType
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data Version
   = V1
   | V2
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 ------------------------------------------------------------------------
 --                       Merge Input / Output                         --
@@ -320,28 +320,28 @@ data Conflict
   | Local Time LocalConflict
   | Global Time GlobalConflict
   | Panic Time String
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data MergeConflict
   = FeatureConflict FeatureId (BothChange FeatureModification)
   | GroupConflict GroupId (BothChange GroupModification)
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data LocalConflict
   = FeatureAlreadyExists FeatureModification FeatureId
   | FeatureNotExists FeatureModification FeatureId
   | GroupAlreadyExists GroupModification GroupId
   | GroupNotExists GroupModification GroupId
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data GlobalConflict
   = FailedDependencies [Dependency]
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data Dependency
   = FeatureDependency FeatureModification FeatureDependencyType
   | GroupDependency GroupModification GroupDependencyType
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data FeatureDependencyType
   = NoChildGroups FeatureId
@@ -349,14 +349,14 @@ data FeatureDependencyType
   | NoCycleFromFeature FeatureId
   | FeatureIsWellFormed FeatureId
   | UniqueName String
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 data GroupDependencyType
   = NoChildFeatures GroupId
   | ParentFeatureExists FeatureId
   | NoCycleFromGroup GroupId
   | GroupIsWellFormed GroupId
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 ------------------------------------------------------------------------
 --                            CLI OPTIONS                             --
@@ -391,54 +391,195 @@ data CliOptions = CliOptions
 customAesonOptions :: Options
 customAesonOptions = defaultOptions{fieldLabelModifier = tail}
 
+-- serialization
+
+instance FromJSON evolutionPlan => FromJSON (MergeInputData evolutionPlan)
+instance ToJSON evolutionPlan => ToJSON (MergeInputData evolutionPlan) where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON ElmDataExamples
 instance ToJSON ElmDataExamples where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON ElmMergeExample
 instance ToJSON ElmMergeExample where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON ElmNamedEvolutionPlan
 instance ToJSON ElmNamedEvolutionPlan where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
-instance ToJSON TreeUserEvolutionPlan where
+-- evolution plans
+
+instance FromJSON featureModel => FromJSON (UserEvolutionPlan featureModel)
+instance ToJSON featureModel => ToJSON (UserEvolutionPlan featureModel) where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
-instance ToJSON (TimePoint TreeFeatureModel) where
+instance FromJSON featureModel => FromJSON (TimePoint featureModel)
+instance ToJSON featureModel => ToJSON (TimePoint featureModel) where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+-- feature models
+
+instance FromJSON TreeFeatureModel
 instance ToJSON TreeFeatureModel where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON TreeFeature
 instance ToJSON TreeFeature where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON TreeGroup
 instance ToJSON TreeGroup where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON FlatFeatureModel
 instance ToJSON FlatFeatureModel where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON FlatFeature
 instance ToJSON FlatFeature where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON FlatGroup
 instance ToJSON FlatGroup where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON FeatureType
 instance ToJSON FeatureType where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
 
+instance FromJSON GroupType
 instance ToJSON GroupType where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+-- conflicts
+
+instance FromJSON Conflict
+instance ToJSON Conflict where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON MergeConflict
+instance ToJSON MergeConflict where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON LocalConflict
+instance ToJSON LocalConflict where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON GlobalConflict
+instance ToJSON GlobalConflict where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON Dependency
+instance ToJSON Dependency where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON FeatureDependencyType
+instance ToJSON FeatureDependencyType where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON GroupDependencyType
+instance ToJSON GroupDependencyType where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance (FromJSON transformation, FromJSON featureModel) => FromJSON (TransformationEvolutionPlan transformation featureModel)
+instance (ToJSON transformation, ToJSON featureModel) => ToJSON (TransformationEvolutionPlan transformation featureModel) where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON transformation => FromJSON (Plan transformation)
+instance ToJSON transformation => ToJSON (Plan transformation) where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON Modifications
+instance ToJSON Modifications where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON FeatureModification
+instance ToJSON FeatureModification where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON FeatureParentModification
+instance ToJSON FeatureParentModification where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON FeatureNameModification
+instance ToJSON FeatureNameModification where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON FeatureTypeModification
+instance ToJSON FeatureTypeModification where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON GroupModification
+instance ToJSON GroupModification where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON GroupParentModification
+instance ToJSON GroupParentModification where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON GroupTypeModification
+instance ToJSON GroupTypeModification where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON modificationType => FromJSON (SingleDiffResult modificationType)
+instance ToJSON modificationType => ToJSON (SingleDiffResult modificationType) where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON modificationType => FromJSON (OneChange modificationType)
+instance ToJSON modificationType => ToJSON (OneChange modificationType) where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON modificationType => FromJSON (BothChange modificationType)
+instance ToJSON modificationType => ToJSON (BothChange modificationType) where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON modificationType => FromJSON (RemovedOrChangedModification modificationType)
+instance ToJSON modificationType => ToJSON (RemovedOrChangedModification modificationType) where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON modificationType => FromJSON (AddedModification modificationType)
+instance ToJSON modificationType => ToJSON (AddedModification modificationType) where
+  toJSON = genericToJSON customAesonOptions
+  toEncoding = genericToEncoding customAesonOptions
+
+instance FromJSON Version
+instance ToJSON Version where
   toJSON = genericToJSON customAesonOptions
   toEncoding = genericToEncoding customAesonOptions
