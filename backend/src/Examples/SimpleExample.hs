e20d262c813432f7310a636e5870113f3881213b
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2021-03-12 15:12:19 +0100

    simple three way merge example code

--- backend/src/Examples/SimpleExample.hs
+++ backend/src/Examples/SimpleExample.hs
@@ -159,3 +159,124 @@ simpleExampleMod =
               (Just (GroupTypeModification Or))
           )
         ]
+
+simpleExampleModV1 :: FlatModificationEvolutionPlan
+simpleExampleModV1 =
+  TransformationEvolutionPlan
+    0
+    initial
+    [Plan 1 modifications1, Plan 2 modifications2]
+  where
+    initial =
+      FlatFeatureModel
+        "rootFeature"
+        [
+          ( "rootFeature"
+          , FlatFeature Nothing Mandatory "Feature 1"
+          )
+        ]
+        []
+    modifications1 =
+      Modifications
+        [
+          ( "feature2"
+          , FeatureAdd "group" Optional "Feature 2"
+          )
+        ,
+          ( "feature3"
+          , FeatureAdd "group" Mandatory "Feature 3"
+          )
+        ,
+          ( "feature4"
+          , FeatureAdd "group" Optional "Feature 4"
+          )
+        ]
+        [("group", GroupAdd "rootFeature" And)]
+    modifications2 =
+      Modifications
+        [ ("feature3", FeatureRemove)
+        ,
+          ( "rootFeature"
+          , FeatureModification
+              Nothing
+              Nothing
+              (Just (FeatureNameModification "Root Feature"))
+          )
+        ]
+        [
+          ( "group"
+          , GroupModification
+              Nothing
+              (Just (GroupTypeModification Alternative))
+          )
+        ]
+
+simpleExampleModV2 :: FlatModificationEvolutionPlan
+simpleExampleModV2 =
+  TransformationEvolutionPlan
+    0
+    initial
+    [Plan 1 modifications1, Plan 2 modifications2]
+  where
+    initial =
+      FlatFeatureModel
+        "rootFeature"
+        [
+          ( "rootFeature"
+          , FlatFeature Nothing Mandatory "Feature 1"
+          )
+        ]
+        []
+    modifications1 =
+      Modifications
+        [
+          ( "feature2"
+          , FeatureAdd "group" Optional "Feature 2"
+          )
+        ,
+          ( "feature3"
+          , FeatureAdd "group" Mandatory "Feature 3"
+          )
+        ]
+        [("group", GroupAdd "rootFeature" And)]
+    modifications2 =
+      Modifications
+        [("feature3", FeatureRemove)]
+        [
+          ( "group"
+          , GroupModification
+              Nothing
+              (Just (GroupTypeModification Or))
+          )
+        ]
+
+simpleExampleMergedPlan :: MergeEvolutionPlan FlatFeatureModel
+simpleExampleMergedPlan =
+  TransformationEvolutionPlan
+    0
+    initial
+    [Plan 1 diffResult1, Plan 2 diffResult2]
+  where
+    initial =
+      FlatFeatureModel
+        "rootFeature"
+        [
+          ( "rootFeature"
+          , FlatFeature Nothing Mandatory "Feature 1"
+          )
+        ]
+        []
+    diffResult1 =
+      DiffResult
+        [ ("feature2", NoChange (FeatureAdd "group" Optional "Feature 2"))
+        , ("feature3", NoChange (FeatureAdd "group" Mandatory "Feature 3"))
+        , ("feature4", ChangedInOne V1 (OneChangeWithoutBase (AddedModification (FeatureAdd "group" Optional "Feature 4"))))
+        ]
+        [("group", NoChange (GroupAdd "rootFeature" And))]
+    diffResult2 =
+      DiffResult
+        [ ("feature3", NoChange FeatureRemove)
+        , ("rootFeature", ChangedInOne V2 (OneChangeWithBase (FeatureModification Nothing Nothing (Just (FeatureNameModification "Root Feature"))) RemovedModification))
+        ]
+        [ ("group", ChangedInOne V1 (OneChangeWithBase (GroupModification Nothing (Just (GroupTypeModification Or))) (ChangedModification (GroupModification Nothing (Just (GroupTypeModification Alternative))))))
+        ]
